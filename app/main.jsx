654b8540983d3a2249725c5be77cfadb10ea7034
Betamee <gongxq95@gmail.com>
2017-02-07 15:08:39 +0800

    add some file

---
+++ app/main.jsx
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom';
+
+const AppRouter=()=>(
+    <h1>
+        Hello World!
+    </h1>
+);
+
+
+
+ReactDOM.render(   
+      <AppRouter/>,
+    document.getElementById('app')
+);
